524bcced469f5fde32d417a58cd8c4bf3a582a44
Kevin Miller <KevM1227@gmail.com>
2018-03-04 18:02:29 -0500

    Converted tabs to spaces. Added example for using axios in react

--- src/App.js
+++ src/App.js
@@ -2,41 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Axios calls should be in components instead of here. Just testing for now
+
+let axios = require('axios')
+
 class App extends Component {
-  state = {
-    response: ''
-  }
+	state = {
+		response: ''
+	}
 
-  componentDidMount() {
-    this.testExpressCall()
-    .then(res => this.setState({ response: res.express }))
-    .catch(err => console.log(err))
-  }
+	componentDidMount() {
+		this.testExpressCall()
+	}
 
-  testExpressCall = async() => {
-    const response = await fetch('/api/test')
-    const body = await response.json()
+	getApiData() {
+		return axios.get('/api/test2')
+	}
 
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
+	testExpressCall = async() => {
+		try {
+			const response = await this.getApiData()
+			this.setState({ response: response.data })
+		} catch (err) {
+			console.error(err)
+			this.setState({ response: err.message })
+		}
+	}
 
-    return body
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.response}
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					{this.state.response}
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
