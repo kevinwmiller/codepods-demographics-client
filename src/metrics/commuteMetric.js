5837284853c9c1b466b60c35b4ad9649645ed2c7
dlaffoo1 <36972908+dlaffoo1@users.noreply.github.com>
2018-05-14 01:07:06 -0400

    refactord commute to use new buildGrid

--- src/metrics/commuteMetric.js
+++ src/metrics/commuteMetric.js
@@ -1,12 +1,14 @@
 import Qs from 'qs'
 import server from '../services/server'
 
+/*
 // These can and probably should be moved to their own file
 const lengthOfLatitudeDegreeAtEquator = 69.172;
 
 function convertToRadians(degreeValue) {
     return (degreeValue * Math.PI) / 180;
 }
+*/
 
 const buildGrid =  require('../map/grid').buildGrid;
 const calculateCorrespondingCell = require('../map/grid').calculateCorrespondingCell;
@@ -44,99 +46,28 @@ export default class {
         }
     }
 
-    /**
-     * Calculates the corresponding cell. TEST ME
-     *
-     * @param      {<type>}  commute     The commute time
-     * @param      {<type>}  cellSize  The cell size
-     * @param      {<type>}  bounds    The bounds
-     * @return     {Array}   The corresponding cell.
-     */
-     calculateCorrespondingCell = (commute, cellSize, bounds, gridSize) => {
-        const topLeftCoordinates = {
-            latitude: bounds.getNorthEast().lat(),
-            longitude: bounds.getSouthWest().lng(),
-        };
-        const distanceFromTopLeft = {
-            // Absolute value since latitude of commute will be smaller than the latitude of the top left corner
-            height: Math.abs(commute.location.latitude - topLeftCoordinates.latitude),
-            width: Math.abs(commute.location.longitude - topLeftCoordinates.longitude),
-        };
-        let cell = [
-            Math.floor(distanceFromTopLeft.height / cellSize.height),
-            Math.floor(distanceFromTopLeft.width / cellSize.width),
-        ]
-        // Doing this to fix any precision errors that are occurring during division.
-        // These error sometimes push the quotient over the "cell boundary" into an invalid cell
-        if (cell[0] >= gridSize.height) {
-            cell[0] = gridSize.height - 1;
-        }
-        if (cell[1] >= gridSize.width) {
-            cell[1] = gridSize.width - 1;
-        }
-        return cell;
-    }
-
-    buildGrid = (commuteData, map) => {
-        const bounds = map.getBounds()
-        const viewportSize = {
-            height: bounds.getNorthEast().lat() - bounds.getSouthWest().lat(),
-            width: bounds.getNorthEast().lng() - bounds.getSouthWest().lng(),
-        };
-        const squareMilesPerCell = 1;
-        // Length of longitude varies depending on your latitude
-        // 1° longitude = cosine (latitude) * length of degree (miles) at equator
-        const lengthOfLongitudeMile = Math.abs(Math.cos(convertToRadians(bounds.getNorthEast().lat())) * lengthOfLatitudeDegreeAtEquator);
-        // 1 degree of latitude is approximately 69 miles
-        // 0.072463781159 degrees of latitude is approximately 5 miles
-        const fiveMilesInLatitudeDegrees = squareMilesPerCell / lengthOfLatitudeDegreeAtEquator;
-        const fiveMilesInLongitudeDegrees = squareMilesPerCell / lengthOfLongitudeMile;
-        const gridSize = {
-            height: Math.ceil(viewportSize.height / fiveMilesInLatitudeDegrees),
-            width: Math.ceil(viewportSize.width / fiveMilesInLongitudeDegrees)
-        };
-        const cellSize = {
-            height: viewportSize.height / gridSize.height,
-            width: viewportSize.width / gridSize.width,
-        }
-        // Create a grid with an empty list in each cell
-        let grid = Array(gridSize.height).fill().map(() => {
-            return Array(gridSize.width).fill().map(() => {
-                return {
-                    cellCenterCoordinates: {
-                        latitude: 0,
-                        longitude: 0,
-                    },
-                    cellData: [],
-                };
-            });
-        });
-
-        // Set center coordinates of each cell. Can probably do this in a cleaner way, but this works for now
-        console.log(bounds);
-        for (let row of grid.keys()) {
-            for (let column of grid[row].keys()) {
-                grid[row][column].cellCenterCoordinates = {
-                    latitude: bounds.getNorthEast().lat() - (row * cellSize.height + (cellSize.height / 2)),
-                    longitude: column * cellSize.width + (cellSize.width / 2) + bounds.getSouthWest().lng(),
-                }
-            }
-        }
+    
+    fillGrid = (commuteData, map) => {
+        const squareMilesPerCell = 0.5;
+        let gridInfo = buildGrid(map, squareMilesPerCell);   
+    
         for (let commute of commuteData) {
-                // Determine grid cell for commute
-                const gridCell = this.calculateCorrespondingCell(commute, cellSize, bounds, gridSize);
-                grid[gridCell[0]][gridCell[1]].cellData.push(commute);
+            // Determine grid cell for commute
+            const gridCell = calculateCorrespondingCell(commute.location, gridInfo.metaData.cellSize, map.getBounds(), gridInfo.metaData.gridSize);
+            gridInfo.grid[gridCell[0]][gridCell[1]].cellData.push(commute.location);
 
-        }
-        return grid;
+    }
+    
+        return gridInfo.grid;
     }
 
+    
     processCommuteData = (googleMaps, commuteData, map, callbacks) => {
         if (!commuteData) {
             return [];
         }
-        let grid = this.buildGrid(commuteData, map);
-        const avgCrimesPerSquareMile = 2;
+        let grid = this.fillGrid(commuteData, map);
+    
         let heatmapData = {"Long": [], "Short": [], "Average": []};
         for (let row of grid.keys()) {
             for (let column of grid[row].keys()) {
@@ -230,7 +161,7 @@ export default class {
 
         this.heatmapAverage = new googleMaps.visualization.HeatmapLayer({
             data: heatmapData["Average"],
-            radius: 200,
+            radius: 400,
             gradient: [
                 'rgba(255, 255, 0, 0)',
                 'rgba(255, 255, 0, 1)',
