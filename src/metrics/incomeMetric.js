3267362120c2ad0f763970758a7761782254545a
Unknown <dave.pan1@gmail.com>
2018-05-14 23:06:08 -0400

    I think it works
    adjusted the values for colorations. It's not perfect, but it shows some data.
    Can add on mouseover for the polygons if we want to show data with it. Also, switching between from this metric to crime crashes.

--- src/metrics/incomeMetric.js
+++ src/metrics/incomeMetric.js
@@ -1,12 +1,16 @@
 import Qs from 'qs';
 import server from '../services/server';
 import geometry_dict from '../map/maryssdict';
+
 export default class {
     constructor() {
         this.incomeData = null;
+        this.polygons = [];
     }
 
     clearData = () => {
+        this.incomeData = null;
+        this.polygons = [];
     }
 
     fetchData = async(bounds) => {
@@ -32,36 +36,56 @@ export default class {
 	for (let i = 0; i < counties.length; ++i) {
             console.log("county " + i + ": " + counties[i]);	
 	}
-        return counties;
+	return counties
     }
 
     updateMap = async (googleMaps, map, callbacks) => {
         console.log("bounds: " + map.getBounds());
-        const countyData = await this.fetchData(map.getBounds());
+        const counties = await this.fetchData(map.getBounds());
         console.log("response: ")
-        console.log(countyData);
-	if (countyData.data.response.length === 0) {
+        console.log(counties);
+	if (counties.data.response.length === 0) {
             console.log("inside a county, two counties needed for comparison");
 		//figure out if we are zoomd inside a given county. 
 	}else{
            console.log(this.incomeData);
 	   const incD = this.incomeData.data.response[0]; // this is a dict
            const incomeData_dict = []
-           for (let i = 0; i < countyData.data.response.length; ++i) { // for each key (county) in dict
-               console.log("name: " + countyData.data.response[i]);
-               console.log("income: " + incD[countyData.data.response[i]]); // 
-//               console.log(
-               console.log("coordinates: " + geometry_dict[countyData.data.response[i]].coordinates);
-               incomeData_dict.push({
-                  key: countyData.data.response[i],
-                  value:[
-		     incD[countyData.data.response[i]],
-		     geometry_dict[countyData.data.response[i]].coordinates,
-		  ],
-	       });
-	   }
-//           incomeData_dict.
+           for (let i = 0; i < counties.data.response.length; ++i) { // for each key (county) in dict
+               // debugging code
+               
+               console.log("name: " + counties.data.response[i]);
+               console.log("income: " + incD[counties.data.response[i]]);
+               const path = geometry_dict[counties.data.response[i]].coordinates;
+               var fillColor = null;
+               var strokeColor = null;
+	       if (incD[counties.data.response[i]] < 45000) {
+	           strokeColor = fillColor = '#FF0000';
 
+	       } else if (incD[counties.data.response[i]] >= 45000 && incD[counties.data.response[i]] < 60000) {
+                   strokeColor = fillColor = '#FFFF00'
+	       } else if (incD[counties.data.response[i]] >=60000) {
+                   strokeColor = fillColor = '#00FF00'
+	       }
+	       var strokeOpacity = .8;
+               var strokeWeight = 2;
+               var fillOpacity = .25;
+               console.log(incomeData_dict);
+               const params = {
+               map,
+               path,
+               strokeColor,
+	       strokeOpacity,
+               strokeWeight,
+               fillColor,
+               fillOpacity
+               
+	       };
+               const polygon = new googleMaps.Polygon(params);
+	       polygon.setMap(map);
+               this.polygons.push(polygon);
+	   }
+	           
 
 
 	}
