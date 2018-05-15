47fbd885e08d7366c191e4bfc40b2937d8d3a7c2
Unknown <dave.pan1@gmail.com>
2018-05-14 21:27:31 -0400

    Data exists in the front end
    still working on the draw, probably have to render based on condition. I made the change, thought there was a clever way out and didn't save what I thought was going to work.

--- src/metrics/incomeMetric.js
+++ src/metrics/incomeMetric.js
@@ -1,54 +1,74 @@
 import Qs from 'qs';
 import server from '../services/server';
-
+import geometry_dict from '../map/maryssdict';
 export default class {
     constructor() {
-
+        this.incomeData = null;
     }
 
     clearData = () => {
     }
 
     fetchData = async(bounds) => {
-        const response = await server.get('/income', {
-            params: {
-                border: {
-                    topRight: {
-                        latitude: bounds.getNorthEast().lat(),
-                        longitude: bounds.getNorthEast().lng(),
-		    },
-                    bottomLeft: {
-                        latitude: bounds.getSouthWest().lat(),
-                        longitude: bounds.getSouthWest().lng(),
-                    },
-		}
-	    },
-            paramsSerializer: function(params) {
-                console.log("Response", Qs.stringify(params, {arrayFormat: 'brackets'}));
-                return Qs.stringify(params, {arrayFormat: 'brackets'});
-            },
-        });
-        return response.data.response
+        const counties = await server.get('/county', { 
+            params: { 
+                border: { 
+                    topRight: { 
+                        latitude: bounds.getNorthEast().lat(), 
+                        longitude: bounds.getNorthEast().lng(), 
+                }, 
+                    bottomLeft: { 
+                        latitude: bounds.getSouthWest().lat(), 
+                        longitude: bounds.getSouthWest().lng(), 
+                }, 
+            } 
+        }, 
+            paramsSerializer: function(params) { 
+                console.log("Response", Qs.stringify(params, {arrayFormat: 'brackets'})); 
+                return Qs.stringify(params, {arrayFormat: 'brackets'}); 
+            }, 
+        });         
+	this.incomeData = await server.get('/income');
+	for (let i = 0; i < counties.length; ++i) {
+            console.log("county " + i + ": " + counties[i]);	
+	}
+        return counties;
     }
 
     updateMap = async (googleMaps, map, callbacks) => {
         console.log("bounds: " + map.getBounds());
-        const incomeData = await this.fetchData(map.getBounds());
-        console.log(incomeData);
-        const kmldata = this.processIncomeData(googleMaps, incomeData, map, callbacks);
-        const kmllayer = new googleMaps.Data();
-	    {/*
-        this.highincome = new googleMaps.visualization.KmlLayer({
-			    url: server.get('
-           
+        const countyData = await this.fetchData(map.getBounds());
+        console.log("response: ")
+        console.log(countyData);
+	if (countyData.data.response.length === 0) {
+            console.log("inside a county, two counties needed for comparison");
+		//figure out if we are zoomd inside a given county. 
+	}else{
+           console.log(this.incomeData);
+	   const incD = this.incomeData.data.response[0]; // this is a dict
+           const incomeData_dict = []
+           for (let i = 0; i < countyData.data.response.length; ++i) { // for each key (county) in dict
+               console.log("name: " + countyData.data.response[i]);
+               console.log("income: " + incD[countyData.data.response[i]]); // 
+//               console.log(
+               console.log("coordinates: " + geometry_dict[countyData.data.response[i]].coordinates);
+               incomeData_dict.push({
+                  key: countyData.data.response[i],
+                  value:[
+		     incD[countyData.data.response[i]],
+		     geometry_dict[countyData.data.response[i]].coordinates,
+		  ],
+	       });
+	   }
+//           incomeData_dict.
+
+
+
 	}
-
-	*/}
-
-
+	
+        	
     }
     processIncomeData = () => {
-        
-        
+       
     }
 }
